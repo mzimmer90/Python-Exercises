9b5454aa69899f6cf1661ddeb47c70273a0cb18d
mzimmer90 <max.zimmer@sky.uk>
2023-02-15 21:42:45 +0000

    Q2

---
+++ Exercise10/Question 2.py
@@ -0,0 +1,37 @@
+PIN = 1234
+attempts = 0
+max_attempts = 3
+
+while attempts < max_attempts:
+    supplied_pin = int(input("Enter your PIN: "))
+    if supplied_pin == PIN:
+        print("You're in!")
+        break
+    else:
+        if attempts < max_attempts:
+            attempts += 1
+            print("Wrong PIN!")
+            if attempts == 3:
+                print("Your account has been locked.")
+            else:
+                print(f"You have {max_attempts-attempts} attempts left.")
+
+
+
+
+
+# PIN = 1234
+# attempts = 3
+#
+# while attempts > 0:
+#     supplied_pin = int(input("Enter your PIN: "))
+#     if supplied_pin == PIN:
+#         print("You're in!")
+#         break
+#     else:
+#         attempts -= 1
+#         if attempts == 0:
+#             print("Your account has been blocked.")
+#         else:
+#             print(f"Incorrect PIN. You have {attempts} attempts left.")
+
