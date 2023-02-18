b0db1758d789be28f625774fd8665cb1d89e912f
PDaly2023 <philip.daly@sky.uk>
2023-02-18 17:43:57 +0000

    ex10_starter reformatted

--- Exercise10/ex10_starter.py
+++ Exercise10/ex10_starter.py
@@ -13,34 +13,29 @@ else:
 pattern = os.path.join(hdir, '*')
 
 
-# TODO: Use the glob.glob() function to obtain the list of filenames
+#Use the glob.glob() function to obtain the list of filenames
+
 print(glob.glob(hdir))
-
 files = glob.glob(pattern)
-
-print("\n", "******files within this folder****")
-#use \n for new line and .join to present each element in list on new line
+print("\n", "******files within this folder****") #use \n for new line and .join to present each element in list on new line
 print("\n" .join(files))
 
-# TODO: use os.path.getsize to find each file's size
-# TODO: Add a test to only display files that do not have a size of zero
-print("")
+
+
+#use os.path.getsize to find each file's size
+#Add a test to only display files that do not have a size of zero
+
 print("\n", "******size of each file****")
-#glob.glob returns a list. getsize expects individual files. iterating over that list gives each individual filea and size.
-for filename in glob.glob(pattern):
-#add test to detect if a file size is 0. if it is. ignore it
-    if os.path.getsize(filename) != 0:
+for filename in glob.glob(pattern): #glob.glob returns a list. getsize expects individual files. iterating over that list gives each individual file and size.
+    if os.path.getsize(filename) != 0: #add test to detect if a file size is 0. if it is. ignore it
         print(os.path.getsize(filename))
 
-# TODO: Remove the leading directory name(s) from each filename before you print it -
-# using os.path.basename()
+
+
+#Remove the leading directory name(s) from each filename before you print it
+
 print("\n", "******each file without it's path****")
-#glob.glob returns a list. os.path.basenameindividual files. iterating over that list gives each individual file without the path
-for filename in glob.glob(pattern):
+for filename in glob.glob(pattern): #glob.glob returns a list. os.path.basenameindividual files. iterating over that list gives each individual file without the path
     print(os.path.basename(filename))
-#print(glob.glob(hdir))
 
-#files = glob.glob(pattern)
-
-#print("******files within this folder****")
-#print(files)
+#bizarre refomatting occuring when adding/removing newlines or empty strings to create newlines. ask Chris :)
